3956b88eaa8eeffc633592ed3436497de0ffd9ed
CK <chamkank@users.noreply.github.com>
2017-11-25 15:43:25 -0800

    add main project starter file

---
+++ Game/air-hockey.hs
@@ -0,0 +1,15 @@
+{- 
+=============================
+CS312 Project 2 - Air Hockey
+=============================
+
+- Written by Harvey, Malinda, Cham
+
+- Uses the FunGEn library 
+    http://www.cin.ufpe.br/~haskell/fungen
+    Copyright (C) 2001  Andre Furtado <awbf@cin.ufpe.br>
+
+- Written using pong example as a reference
+    http://fungen.joyful.com/site/example.html
+
+-}
