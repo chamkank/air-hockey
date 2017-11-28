00006ef23b4a555a8c0ac3f93a691bb348fb8e45
CK <chamkank@users.noreply.github.com>
2017-11-27 21:52:01 -0800

    add main method and game loop

--- Game/air-hockey.hs
+++ Game/air-hockey.hs
@@ -22,9 +22,11 @@ import Paths_FunGEn (getDataFileName)
 
 -- window settings
 window_width = 400
-window_height = 1000
+window_height = 600
 w = fromIntegral window_width :: GLdouble
 h = fromIntegral window_height :: GLdouble
+win_x_pos = 500
+win_y_pos = 10
 window_title = "Air Hockey"
 
 -- background constants
@@ -33,4 +35,27 @@ background_tile_width = 256
 background_tile_height = 256
 
 -- game constants
-data GameAttribute = Score Int -- define algebraic type for Score
+data GameAttribute = Score Int Int -- define algebraic type for Score
+fps_delay = 30
+
+-- initialize game
+main :: IO ()
+main = do
+  background_tile <- getDataFileName background_file_path                       -- getDataFileName :: FilePath -> IO FilePath
+  let win_config = ((100, 80), (window_width, window_height), window_title)
+      img_list = [(background_tile, Nothing)]                                   -- [(FilePath, InvList)] (we don't need InvList)
+      game_map = textureMap 0 background_tile_width background_tile_height w h  -- create a PreTextureMap to cover the screen
+
+      -- create object groups (currently empty)
+      strikers = objectGroup "strikerGroup" []                                   
+      puck = objectGroup "puckGroup" []                                         
+      
+      game_score = Score 0 0                                                    -- initialize game score to 0 for both players
+      input_map = []                                                            -- define key mapping to functions
+  funInit win_config game_map [strikers, puck] () game_score input_map gameCycle (Timer fps_delay) img_list -- initialize game with defined properties above
+
+-- game loop
+gameCycle :: IOGame GameAttribute () () () ()
+gameCycle = do
+    printOnScreen (show 5) TimesRoman24 (0,0) 1.0 1.0 1.0
+    showFPS TimesRoman24 (w-40,0) 1.0 0.0 0.0
