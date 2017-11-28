8a204e2fe64952b40f06530c4d524a855906b2a6
CK <chamkank@users.noreply.github.com>
2017-11-27 23:19:22 -0800

    add puck object (at center of screen)

--- Game/air-hockey.hs
+++ Game/air-hockey.hs
@@ -37,6 +37,7 @@ background_tile_height = 256
 -- game constants
 data GameAttribute = Score Int Int -- define algebraic type for Score
 fps_delay = 30
+puck_radius = 15.0
 
 -- initialize game
 main :: IO ()
@@ -47,12 +48,20 @@ main = do
       game_map = textureMap 0 background_tile_width background_tile_height w h  -- create a PreTextureMap to cover the screen
 
       -- create object groups (currently empty)
-      strikers = objectGroup "strikerGroup" []                                   
-      puck = objectGroup "puckGroup" []                                         
+      strikers = objectGroup "strikerGroup" []
+      puck = objectGroup "puckGroup" [createPuck]                                    
       
       game_score = Score 0 0                                                    -- initialize game score to 0 for both players
       input_map = []                                                            -- define key mapping to functions
-  funInit win_config game_map [strikers, puck] () game_score input_map gameCycle (Timer fps_delay) img_list -- initialize game with defined properties above
+  
+  -- initialize game with defined properties above
+  funInit win_config game_map [strikers, puck] () game_score input_map gameCycle (Timer fps_delay) img_list
+
+
+createPuck :: GameObject ()
+createPuck = 
+  let puck_pic = Basic (Circle puck_radius 0.0 1.0 0.0 Filled)
+  in object "puck" puck_pic False (w/2, h/2) (0, 0) ()
 
 -- game loop
 gameCycle :: IOGame GameAttribute () () () ()
