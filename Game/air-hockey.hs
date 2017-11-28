515db18ca1fd14478c34104948b3f175eb9192c3
CK <chamkank@users.noreply.github.com>
2017-11-27 17:37:04 -0800

    initialize imports & window settings

--- Game/air-hockey.hs
+++ Game/air-hockey.hs
@@ -13,3 +13,22 @@ CS312 Project 2 - Air Hockey
     http://fungen.joyful.com/site/example.html
 
 -}
+
+-- import libraries
+module Main where
+import Graphics.UI.Fungen
+import Graphics.Rendering.OpenGL (GLdouble)
+import Paths_FunGEn (getDataFileName)
+
+-- window settings
+window_width = 400
+window_height = 1000
+w = fromIntegral window_width :: GLdouble
+h = fromIntegral window_height :: GLdouble
+
+
+data GameAttribute = Score Int -- define algebraic type for Score
+
+
+
+
